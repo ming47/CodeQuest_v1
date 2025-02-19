f4919159ede42ecf737690bad20105630da0a83b
tester <test@test.com>
2025-02-19 13:57:22 +0900

    랭킹 서비스 구현

--- src/main/java/DTO/ScoreDTO.java
+++ src/main/java/DTO/ScoreDTO.java
@@ -9,16 +9,27 @@ public class ScoreDTO {
 	private int scoreId;
 	private int gameId;
 	private int memberId;
-	private int score;
+	private double score;
 	private Timestamp regDate;
+	private String user;
 	
-	public ScoreDTO(int scoreId, int gameId, int memberId, int score, Timestamp regDate) {
+	public ScoreDTO(int gameId, int memberId, double score, Timestamp regDate, String user) {
+		super();
+		this.gameId = gameId;
+		this.memberId = memberId;
+		this.score = score;
+		this.regDate = regDate;
+		this.user = user;
+	}
+	
+	public ScoreDTO(int scoreId, int gameId, int memberId, double score, Timestamp regDate, String user) {
 		super();
 		this.scoreId = scoreId;
 		this.gameId = gameId;
 		this.memberId = memberId;
 		this.score = score;
 		this.regDate = regDate;
+		this.user = user;
 	}
 	
 	public ScoreDTO() {
@@ -43,10 +54,10 @@ public class ScoreDTO {
 	public void setMemberId(int memberId) {
 		this.memberId = memberId;
 	}
-	public int getScore() {
+	public double getScore() {
 		return score;
 	}
-	public void setScore(int score) {
+	public void setScore(double score) {
 		this.score = score;
 	}
 	public Timestamp getRegDate() {
@@ -58,10 +69,10 @@ public class ScoreDTO {
 	
 	public static ScoreDTO of(ResultSet rs) throws SQLException {
 		return new ScoreDTO(
-				rs.getInt("SCORE_ID"),
 				rs.getInt("GAME_ID"),
 				rs.getInt("MEMBER_ID"),
-				rs.getInt("SCORE"),
-				rs.getTimestamp("REG_DATE"));
+				rs.getDouble("SCORE"),
+				rs.getTimestamp("REG_DATE"),
+				rs.getString("NICKNAME"));
 	}
 }
