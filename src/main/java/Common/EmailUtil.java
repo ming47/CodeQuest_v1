c5aaca0c3a699c00b5a60d7d4b91d9a87957e3e6
qhfka74 <shj04367@gmail.com>
2025-02-21 14:49:26 +0900

    email_send_v2

--- src/main/java/Common/EmailUtil.java
+++ src/main/java/Common/EmailUtil.java
@@ -16,6 +16,7 @@ public class EmailUtil {
             Properties props = new Properties();
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.ssl.protocols", "TLSv1.2");
             props.put("mail.smtp.host", "smtp.gmail.com");
             props.put("mail.smtp.port", "587");
 
